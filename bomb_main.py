a40948cc0864f2b34b6916968e5acbb442dfaf22
mfy0303 <mfy0303@126.com>
2022-05-15 13:42:03 +0800

    you can see the bomb now

--- bomb_main.py
+++ bomb_main.py
@@ -1,5 +1,5 @@
 import pygame
-import sys 
+import random 
 import find_bomb
 from pygame.locals import*
 
@@ -11,9 +11,11 @@ clock=pygame.time.Clock()
 bg_color = (230,230,230)
 square_index_x=0
 square_index_y=0
+bomb_pos=[]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("ATTENTION!THERE ARE MANY BOMBS!")
 square_flag=True
+blit_square=0
 def squares(group):
 	global w
 	global h
@@ -33,9 +35,20 @@ def mouse_pos_square(a,b):
 	elif r*65 < a:
 		if u*65 < b:
 			return r+1,u+1
+def plant_bomb(x,y):
+	one_bomb_pos=[]
+	uptctb1=random.randint(1,x)
+	uptctb2=random.randint(1,y)
+	one_bomb_pos.append(uptctb1)
+	one_bomb_pos.append(uptctb2)
+	return one_bomb_pos
 add_square=pygame.sprite.Group()
 squares(add_square)
 running=True
+for i in range(10):
+	r=plant_bomb(8,8)
+	bomb_pos.append(r)
+print(bomb_pos)	
 while running:
 	screen.fill(bg_color)
 	event = pygame.event.poll()
@@ -56,8 +69,16 @@ while running:
 		if square_flag == False:
 			if x3==square_index_x:
 				if y3==square_index_y:
-					each.image=pygame.image.load("no_bomb.png").convert_alpha()
-					square_flag=True	
+					for i in range(10):
+						if x3==bomb_pos[i][0] and y3==bomb_pos[i][1]:
+							each.image=pygame.image.load("bomb.png").convert_alpha()
+							blit_square=0
+						else:
+								blit_square=blit_square+1
+						if blit_square == 10:
+							each.image=pygame.image.load("no_bomb.png").convert_alpha()
+							blit_square=0
+					square_flag=True
 	pygame.display.flip()
 	clock.tick(120)	
 	
